02ba4af4f70f8da0ed4d7145d5fc3e55ad231d88
chinnalaa485 <82211301+chinnalaa485@users.noreply.github.com>
2021-04-10 13:51:12 +0530

    WhatToEat.c
    So in this program dispalys the food item and price of it as soon as you enter a number.

---
+++ WhatToEat.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int n;
+Printf("enter the number");
+Scanf("%d",&n);
+switch(n)
+{
+Case(1):
+Printf("Food item-Pizza");
+Printf("\nPrice-Rs239");
+Break;
+Case(2):
+Printf("Food item-Burger");
+Printf("\nPrice-Rs129");
+Break;
+Case(3):
+Printf("Food item-Pasta");
+Printf("\nPrice-Rs179");
+Break;
+Case(4):
+Printf("Food item-FrenchFries");
+Printf("\nPrice-Rs99");
+Break;
+Case(5):
+Printf("Food item-Sandwich");
+Printf("\nPrice-Rs149");
+Break;
+Default:
+Printf("not found");
+Break;
+}
+}
