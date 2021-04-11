ac688051261384ae5ccdc2affb283bae1d10ecb0
chinnalaa485 <82211301+chinnalaa485@users.noreply.github.com>
2021-04-11 11:53:12 +0530

    Grade.c
    This file dispalys the grade obtained by the student

---
+++ Grade.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int m=0;
+Printf("enter the marks",m);
+Scanf("%d",&m);
+if(m>=85 && m<=100);
+{
+       Printf("GradeA");
+}
+elif(m>=70 && m<=84);
+{
+       Printf("GradeB");
+}
+elif(m>=55 && m<=69);
+{
+       Printf("GradeC");
+}
+elif(m>=40 && m<=54);
+{
+       Printf("GradeD");
+}
+else(m<40);
+{
+      printf("grade f");
+}
+}
